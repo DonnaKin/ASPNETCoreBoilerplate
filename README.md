fcabbfff8c8dd76ac5c5168b6ead66adcec734ec
김민경 <donna@tobecon.co.kr>
2024-04-01 16:07:15 +0900

    bootstrap sh 파일 수정

--- README.md
+++ README.md
@@ -21,6 +21,9 @@ origin <your githhub repository url> (push)
 ## 2. bootstrap을 실행시켜 프로젝트 이름을 바꿔준다.
 
 ### bash
+> bootstrap.sh 스크립트의 sed 명령어는 macOS에서 사용할 때 빈 문자열("")을 옵션으로 추가하여야합니다.
+> 참고 : https://stackoverflow.com/questions/19456518/sed-command-with-i-option-failing-on-mac-but-works-on-linux
+> 다른 운영체제에서 문제가 발생한다면 ""를 제거하고 실행합니다.
 
 ```
 // bootstrap.sh에 대한 실행 권한을 줍니다.
--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -16,11 +16,14 @@ mv "${old_name}.sln" "${new_name}.sln"
 mv "${old_name}.csproj" "${new_name}.csproj"
 
 # 파일 내의 'CoreBoilerplate'를 새 프로젝트 이름으로 변경
-sed -i "s/${old_name}/${new_name}/g" README.md
-sed -i "s/${old_name}/${new_name}/g" Pages/_ViewImports.cshtml
-sed -i "s/${old_name}/${new_name}/g" Pages/Error.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" Pages/Index.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" Pages/Privacy.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" Pages/Shared/_Layout.cshtml
+# macos에서는 sed 명령어 뒤에 ""를 붙여야 함
+# https://stackoverflow.com/questions/19456518/sed-command-with-i-option-failing-on-mac-but-works-on-linux
+# 다른 운영체제에서 문제가 발생한다면 ""를 제거하고 실행
+sed -i "" "s/${old_name}/${new_name}/g" README.md
+sed -i "" "s/${old_name}/${new_name}/g" Pages/_ViewImports.cshtml
+sed -i "" "s/${old_name}/${new_name}/g" Pages/Error.cshtml.cs
+sed -i "" "s/${old_name}/${new_name}/g" Pages/Index.cshtml.cs
+sed -i "" "s/${old_name}/${new_name}/g" Pages/Privacy.cshtml.cs
+sed -i "" "s/${old_name}/${new_name}/g" Pages/Shared/_Layout.cshtml
 
-echo "Project files have been updated."
+echo "Project files and README have been updated."
