97d82972be912ed8b50459036235816f09e598ea
김민경 <donna@tobecon.co.kr>
2024-04-01 15:40:49 +0900

    bootstrap 내용 변경

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 프로젝트의 루트 디렉토리로 이동
+cd ..
+
 # 사용자로부터 새 프로젝트 이름 입력받기
 read -p "Enter the new project name: " new_name
 
@@ -7,17 +10,17 @@ read -p "Enter the new project name: " new_name
 old_name="CoreBoilerplate"
 
 # 솔루션(.sln) 파일 이름 변경
-mv "../${old_name}.sln" "../${new_name}.sln"
+mv "${old_name}.sln" "${new_name}.sln"
 
 # 프로젝트(.csproj) 파일 이름 변경
-mv "../${old_name}.csproj" "../${new_name}.csproj"
+mv "${old_name}.csproj" "${new_name}.csproj"
 
 # 파일 내의 'CoreBoilerplate'를 새 프로젝트 이름으로 변경
-sed -i "s/${old_name}/${new_name}/g" ../README.md
-sed -i "s/${old_name}/${new_name}/g" ../Pages/_ViewImports.cshtml
-sed -i "s/${old_name}/${new_name}/g" ../Pages/Error.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" ../Pages/Index.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" ../Pages/Privacy.cshtml.cs
-sed -i "s/${old_name}/${new_name}/g" ../Pages/Shared/_Layout.cshtml
+sed -i "s/${old_name}/${new_name}/g" README.md
+sed -i "s/${old_name}/${new_name}/g" Pages/_ViewImports.cshtml
+sed -i "s/${old_name}/${new_name}/g" Pages/Error.cshtml.cs
+sed -i "s/${old_name}/${new_name}/g" Pages/Index.cshtml.cs
+sed -i "s/${old_name}/${new_name}/g" Pages/Privacy.cshtml.cs
+sed -i "s/${old_name}/${new_name}/g" Pages/Shared/_Layout.cshtml
 
 echo "Project files have been updated."
